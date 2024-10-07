d6eccd689e9f8e009284b314466730f0c4d270c3
k4yt3x <i@k4yt3x.com>
2024-10-07 00:00:00 +0000

    feat: initial commit
    Signed-off-by: k4yt3x <i@k4yt3x.com>

---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionExit_triggered();
+
+    void on_addFilesPushButton_clicked();
+
+    void on_deletedSelectedPushButton_clicked();
+
+    void on_clearPushButton_clicked();
+
+    void on_startPausePushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
